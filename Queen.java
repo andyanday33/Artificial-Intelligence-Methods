ad0fd244c210e2e53b3cb2d7ab147d6e5138c4a8
Berke Anday Baloglu <31995815+andyanday33@users.noreply.github.com>
2020-04-24 20:35:00 +0300

    Create Queen.java

---
+++ Queen.java
@@ -0,0 +1,60 @@
+package yzproje1;
+import java.math.*;
+/**
+ *
+ * @author pc
+ */
+public class Queen implements Cloneable{
+    
+    private int row;
+    private int column;
+    
+    public Queen(int row,int column){
+        this.row = row;
+        this.column = column;
+    }
+    
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+    
+    public boolean conflict (Queen a){
+        
+        if(this.row == a.row){
+            return true;
+            
+        }
+        if(Math.abs(this.row - a.row) == Math.abs(this.column - a.column)){
+            return true;
+        }
+        
+       
+        return false;
+    }
+    
+    /*public void moveUp(){
+        row++;
+    }
+    public void moveDown(){
+        row--;
+    }*/
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
+    
+}
